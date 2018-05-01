655eef3737d78fe2369b58c3e5353ff520707d9e
Virak Ngauv <twohappypancakes@gmail.com>
2018-05-01 12:36:30 -0400

    Initial Commit

---
+++ Wake Up With/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Wake Up With
+//
+//  Created by Virak Ngauv on 5/1/18.
+//  Copyright © 2018 Virak Ngauv. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

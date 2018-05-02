07a29d4afb656de48ed9d933bc2f1f91728513d6
Virak Ngauv <twohappypancakes@gmail.com>
2018-05-02 13:58:53 -0400

    Incorporate Firebase/CocoaPods and Add Log In buttons and Log In with Email view

---
+++ Wake Up With/RegisterViewController.swift
@@ -0,0 +1,29 @@
+//
+//  RegisterViewController.swift
+//  Wake Up With
+//
+//  Created by Virak Ngauv on 5/1/18.
+//  Copyright © 2018 Virak Ngauv. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class RegisterViewController: UIViewController {
+    @IBOutlet weak var googleLogInButton: UIButton!
+    @IBOutlet weak var facebookLogInButton: UIButton!
+    @IBOutlet weak var emailLogInButton: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.navigationController?.navigationBar.isHidden = true
+    }
+    
+    
+    @IBAction func logInWithEmail(_ sender: UIButton) {
+        
+    }
+    
+}

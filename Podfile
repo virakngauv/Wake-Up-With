39bab0cd1262862e6a8e61a8fcf44c6abae40171
Virak Ngauv <twohappypancakes@gmail.com>
2018-05-08 12:07:05 -0400

    Add Facebook log in capability and add Podfile & Podfile.lock to control

---
+++ Podfile
@@ -0,0 +1,15 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'Wake Up With' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Wake Up With
+
+pod 'Firebase/Core'
+pod 'FirebaseUI/Auth'
+pod 'FirebaseUI/Google'
+pod 'FirebaseUI/Facebook'
+
+end
--- Wake Up With/RegisterViewController.swift
+++ Wake Up With/RegisterViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import Firebase
+import FBSDKLoginKit
 
 class RegisterViewController: UIViewController {
     @IBOutlet weak var googleLogInButton: UIButton!
@@ -23,6 +24,31 @@ class RegisterViewController: UIViewController {
     
     override func viewWillAppear(_ animated: Bool) {
         self.navigationController?.navigationBar.isHidden = true
+    }
+    
+    @IBAction func logInWithFacebook(_ sender: UIButton) {
+        FBSDKLoginManager().logIn(withReadPermissions: ["email"], from: self, handler: { (result, error) in
+            
+            print(result)
+            
+            guard result != nil, !(result?.isCancelled)!, error == nil else { return  FBSDKLoginManager().logOut()}
+            if let accessToken = result?.token.tokenString {
+                let credential = FacebookAuthProvider.credential(withAccessToken: accessToken)
+                Auth.auth().signIn(with: credential) { (user, error) in
+                    if let error = error {
+                        print(error.localizedDescription)
+                        let alert = UIAlertController(title: "error", message: "\(error.localizedDescription)", preferredStyle: .alert)
+                        let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+                        alert.addAction(okAction)
+                    }else{
+                        DispatchQueue.main.async {
+                            print(123)
+                        }
+                    }
+                }
+            }
+            
+        })
     }
     
     
